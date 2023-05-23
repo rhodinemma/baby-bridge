5e04b5ad238842537b1e9fe8838a6fb6180ff070
rhodinemma <rhodinemma10@gmail.com>
2023-05-23 22:30:03 +0300

    feat: add connect buttons to list items plus alertDialog

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -11,6 +11,20 @@ class _DashboardState extends State<Dashboard>
     with SingleTickerProviderStateMixin {
   TabController? _tabController;
 
+  final List<String> intendedParents = [
+    'Intended Parent 1',
+    'Intended Parent 2',
+    'Intended Parent 3',
+    'Intended Parent 4',
+  ];
+
+  final List<String> surrogateMothers = [
+    'Surrogate Mother 1',
+    'Surrogate Mother 2',
+    'Surrogate Mother 3',
+    'Surrogate Mother 4',
+  ];
+
   @override
   void initState() {
     super.initState();
@@ -21,6 +35,98 @@ class _DashboardState extends State<Dashboard>
   void dispose() {
     _tabController?.dispose();
     super.dispose();
+  }
+
+  void _showConnectModalForSurrogates(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext dialogContext) {
+        return AlertDialog(
+          title: const Text('Connect to Intended Parent', style: TextStyle(fontWeight: FontWeight.bold),),
+          content: DropdownButton<String>(
+            value: intendedParents.first,
+            items: intendedParents.map((String parent) {
+              return DropdownMenuItem<String>(
+                value: parent,
+                child: Text(parent),
+              );
+            }).toList(),
+            onChanged: (String? selectedParent) {
+              // Handle the selected parent
+              if (selectedParent != null) {
+                // Perform the connection logic here
+              }
+            },
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(dialogContext);
+              },
+              child: const Text('Cancel'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Perform the connection logic here
+              },
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(const Color(0xFFFFBF9B)),
+              ),
+              child: const Text(
+                'Connect',
+                style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showConnectModalForParents(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext dialogContext) {
+        return AlertDialog(
+          title: const Text('Connect to Surrogate Mother', style: TextStyle(fontWeight: FontWeight.bold),),
+          content: DropdownButton<String>(
+            value: surrogateMothers.first,
+            items: surrogateMothers.map((String parent) {
+              return DropdownMenuItem<String>(
+                value: parent,
+                child: Text(parent),
+              );
+            }).toList(),
+            onChanged: (String? selectedParent) {
+              // Handle the selected parent
+              if (selectedParent != null) {
+                // Perform the connection logic here
+              }
+            },
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(dialogContext);
+              },
+              child: const Text('Cancel'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Perform the connection logic here
+              },
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(const Color(0xFFFFBF9B)),
+              ),
+              child: const Text(
+                'Connect',
+                style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -65,8 +171,22 @@ class _DashboardState extends State<Dashboard>
                     itemBuilder: (context, index) {
                       return Card(
                         child: ListTile(
-                          title: Text('Surrogate ${index + 1}'),
+                          title: Text(
+                            'Surrogate ${index + 1}',
+                            style: const TextStyle(fontWeight: FontWeight.bold),
+                          ),
                           subtitle: const Text('Surrogate details'),
+                          trailing: ElevatedButton(
+                            onPressed: () {
+                              _showConnectModalForSurrogates(context);
+                            },
+                            style: ButtonStyle(
+                              backgroundColor:
+                              MaterialStateProperty.all(const Color(0xFFFFBF9B)),
+                              minimumSize: MaterialStateProperty.all(const Size(100, 40)),
+                            ),
+                            child: const Text('Connect', style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),),
+                          ),
                         ),
                       );
                     },
@@ -77,8 +197,21 @@ class _DashboardState extends State<Dashboard>
                     itemBuilder: (context, index) {
                       return Card(
                         child: ListTile(
-                          title: Text('Intended Parent ${index + 1}'),
+                          title: Text('Intended Parent ${index + 1}',
+                              style:
+                                  const TextStyle(fontWeight: FontWeight.bold)),
                           subtitle: const Text('Intended Parent details'),
+                          trailing: ElevatedButton(
+                            onPressed: () {
+                              _showConnectModalForParents(context);
+                            },
+                            style: ButtonStyle(
+                              backgroundColor:
+                              MaterialStateProperty.all(const Color(0xFFFFBF9B)),
+                              minimumSize: MaterialStateProperty.all(const Size(100, 40)),
+                            ),
+                            child: const Text('Connect', style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),),
+                          ),
                         ),
                       );
                     },
